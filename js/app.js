1122131b8cdf537773a9727c18780533c159346e
Marco <marco@magrini.se>
2015-09-22 01:40:21 +0200

    added backbone.radio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-var mdl = require('material-design-lite');
+require('material-design-lite');
+require('react');
+require('backbone.radio');
+
+Backbone.Radio.tuneIn('data');
+Backbone.Radio.trigger('data', 'some:event');
